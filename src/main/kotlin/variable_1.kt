0aafa14900c67fe2f9cd1eb755a293bc2f3a5fa6
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-10-21 15:48:20 +0900

    변수

---
+++ src/main/kotlin/variable_1.kt
@@ -0,0 +1,30 @@
+var x = 5
+
+fun main() {
+    x += 1
+    println(x)
+
+    val a: Int = 1
+
+    val b = 1
+
+    val c: Int
+    c = 3
+
+    val d: Int
+    d = 123
+
+    //val(value)
+    //var(variable)
+
+    var e: String = "Hello"
+    e = "Word"
+
+    var f = 123
+//    f = "hi"
+
+
+
+
+
+}
