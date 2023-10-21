3a40f9f66866f9b825917ef9e3a134f86d8d795b
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-10-21 17:31:57 +0900

    예외처리

---
+++ src/main/kotlin/5_exception.kt
@@ -0,0 +1,34 @@
+fun main() {
+    Thread.sleep(1)
+
+    try {
+        Thread.sleep(1)
+    } catch (e: Exception) {
+        println("에러 발생!")
+    } finally {
+        println("finally 실행!")
+    }
+
+    val a = try {
+        "1234".toInt()
+    } catch (e: Exception) {
+        println("예외 발생!")
+    }
+
+    println(a)
+
+//    throw Exception("예외 발생!")
+
+    val b: String? = "널이 아님"
+    val c: String = b ?: failFast("a is null")
+
+    println(c.length)
+
+    failFast("예외 발생!")
+
+    println("이 메시지는 출력될까?")
+}
+
+fun failFast(message: String): Nothing {
+    throw IllegalArgumentException(message)
+}
